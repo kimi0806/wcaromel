1a9ebb5ea49aa62d0a7c23332705d824d370c473
Romel Sallutal <kimi@Romels-Air.home>
2021-10-18 19:54:45 +0200

    composer files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a035db2c04a02a86012215d4b313c1a946084914',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a035db2c04a02a86012215d4b313c1a946084914',
+            'dev_requirement' => false,
+        ),
+    ),
+);
